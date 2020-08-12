6b75e0e40913c61e2f6450533d74fcbd0ed46fc4
Aidos <aidos_ka@yahoo.com>
2020-08-12 16:02:59 -0500

    Add timer logic, needs to be debbuged

--- src/components/Board/Cards/Timer/index.js
+++ src/components/Board/Cards/Timer/index.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 export const Timer = ({ toggle }) => {
-  return <p>{toggle}</p>;
+  const [time, setTime] = useState(0);
+  // we are setting up a setInterval while the toggle === true
+  useEffect(() => {
+    while (toggle) {
+      const intervalID = setInterval(() => {
+        setTime((time) => time + 1);
+      }, 1000);
+      //  Cleanup function
+      return () => {
+        clearInterval(intervalID);
+      };
+    }
+  });
+
+  return <p>{time}</p>;
 };
 
 Timer.propTypes = { toggle: PropTypes.bool };
 
 Timer.defaultProps = {
-  toggle: false
-}
+  toggle: false,
+};
